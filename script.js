2ff4371d7887712ecee38574da37d1d603974468
MukthiKaup <70291745+MukthiKaup@users.noreply.github.com>
2020-10-09 19:09:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+let allMovies = [];
+
+//Movie object contructor
+function Movie(title, rating, havewatched) {
+    this.title = title;
+    this.rating = rating;
+    this.havewatched = havewatched;
+}
+
+//add a movie OBJECT to the allMovies array
+function addMovie (movie) {
+    var movie1 = movie1;
+    allMovies.push(movie);
+    console.log("A new movie is added");
+}
+
+//iterate through all elements of allMovies array
+//print out to console in a correct format
+//print out the total number of movies in allMovies array
+function printMovies () {
+    console.log("Printing all movies....");
+    for (var i=0; i <= allMovies.length - 1; i++){
+        console.log(allMovies[i].title + ", rating of " + allMovies[i].rating + ", havewatched: " + allMovies[i].havewatched);
+    }
+    console.log("")
+    console.log("You have " + allMovies.length + " movies in total");
+       
+}
+
+
+//print out to console, only the movies that has a rating higher than rating(argument)
+//print out the total number of matches
+function highRatings (rating) {
+    console.log("printing movie that has a rating higher than " + rating)
+    var counter = 0;
+    for (var i=0; i <= allMovies.length - 1; i++){
+        if (allMovies[i].rating > rating){
+            console.log(allMovies[i].title + " has a rating of " + allMovies[i].rating);
+                counter++;
+        }
+    
+    }
+    console.log("")
+    console.log("In total, there are " + counter + " matches");
+}
+
+//Toggle the 'haveWatched' property of the specified movie 
+function changeWatched (title) {
+    for (var i=0; i <= allMovies.length-1; i++){
+        if (title == allMovies[i].title){
+            if (allMovies[i].havewatched == "true"){
+                allMovies[i].havewatched = "false"
+            }
+            else if (allMovies[i].havewatched == "false"){
+                allMovies[i].havewatched = "true"
+            }
+        }
+            
+    }
+    console.log("changing the status of the movie...");
+}
+
+
+
+////////////////////////////////////////////////////////////
+//Test code - DO NOT DELETE OR EDIT
+let x = new Movie("Spiderman", 3, "true");
+let y = new Movie("Citizen Kane", 4, "false");
+let z = new Movie("Zootopia", 4.5, "true");
+
+allMovies.push(x,y,z);
+
+console.log("----------------");
+console.log("running program......");
+console.log("----------------");
+printMovies();
+
+
+let movie1 = new Movie("Parasite", 2, "false");
+
+
+console.log("----------------");
+addMovie(movie1);
+console.log("----------------");
+
+
+
+changeWatched("Spiderman");
+console.log("----------------");
+
+printMovies();
+console.log("----------------");
+
+changeWatched("Spiderman");
+console.log("----------------");
+
+printMovies();
+console.log("----------------");
+
+highRatings(3.5);
